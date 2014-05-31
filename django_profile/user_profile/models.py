a75beaa4f6be407c831b7167524d9bf0c074493d
k1-hedayati <k1.hedayati93@gmail.com>
2014-05-31 23:31:57 +0430

    Created project

---
+++ django_profile/user_profile/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ProfileUser(models.Model):
+    user = models.OneToOneField(User)
+    bio = models.TextField()
+    skills = models.ManyToManyField(Skill)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    birthday = models.DateTimeField(blank=True, null=True)
+    sessions = models.IntegerField()
+
+# class Friends(models.Model):
+#     userF = models.ForeignKey(PugUsers, related_name="userF")
+#     userFriend = models.ForeignKey(PugUsers, related_name="user_Friend")
+#     friendshipDate = models.DateTimeField(auto_now=True)
+
+# class Message(models.Model):
+#     userSender = models.ForeignKey(PugUsers, related_name="user_sender")
+#     userReceiver = models.ForeignKey(PugUsers, related_name="user_receiver")
+#     mesSubject = models.CharField(max_length=100)
+#     mesBody = models.CharField(max_length=1000)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Social(models.Model):
+    user = models.ForeignKey(ProfileUser)
+    site = models.IntegerField(max_length=20)
+    user_link = models.URLField(max_length=70)
+
+
+class Presentaion(models.Model):
+    user = models.ForeignKey(ProfileUser)
+    presentaion_link = models.URLField(max_length=70)
+    like = models.IntegerField()
