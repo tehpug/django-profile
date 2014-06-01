37cbf6ede8f355728ed37e5a43029587612ef19a
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-06-01 21:23:37 +0430

    automatic profile creation test added

--- django_profile/user_profile/tests.py
+++ django_profile/user_profile/tests.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
+
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    def test_creating_user_will_create_profile(self):
+        user = User(username='test', email='test@test.com')
+        user.set_password('test')
+        user.save()
+        self.assertIsNotNone(user.id)
+        self.assertIsNotNone(user.profile)
+        self.assertIs(0, user.profile.sessions)
+
