cedeb5c3ad2c21bebd72c0121346738acca84945
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-06-01 21:25:25 +0430

    Skill class moved to top, because UserProfile need it ;) I think ProfileUser is worng so i changed it to UserProfile. An interceptor for automatic profile creation added. and a litte improvement in UserProfile model

--- django_profile/user_profile/models.py
+++ django_profile/user_profile/models.py
@@ -1,14 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
-class ProfileUser(models.Model):
-    user = models.OneToOneField(User)
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,related_name='profile')
     bio = models.TextField()
     skills = models.ManyToManyField(Skill)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     birthday = models.DateTimeField(blank=True, null=True)
-    sessions = models.IntegerField()
+    sessions = models.IntegerField(default=0)
+
+
+########## An interceptor to create profile for every user who is created automatically
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.get_or_create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
+#######################################################################################
+
 
 # class Friends(models.Model):
 #     userF = models.ForeignKey(PugUsers, related_name="userF")
@@ -22,17 +36,17 @@ class ProfileUser(models.Model):
 #     mesBody = models.CharField(max_length=1000)
 
 
-class Skill(models.Model):
-    name = models.CharField(max_length=50)
 
 
 class Social(models.Model):
-    user = models.ForeignKey(ProfileUser)
+    user = models.ForeignKey(UserProfile)
     site = models.IntegerField(max_length=20)
     user_link = models.URLField(max_length=70)
 
 
 class Presentaion(models.Model):
-    user = models.ForeignKey(ProfileUser)
+    user = models.ForeignKey(UserProfile)
     presentaion_link = models.URLField(max_length=70)
     like = models.IntegerField()
+
+
