2e0c9aadac1bbf011d8dff258554dd9f53ae7bc2
nima <nima@localhost.localdomain>
2014-06-03 10:51:43 +0430

    register current models in django admin

--- django_profile/user_profile/admin.py
+++ django_profile/user_profile/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from models import Social,Presentaion,UserProfile,Skill
 # Register your models here.
+admin.site.register(Social)
+admin.site.register(Presentaion)
+admin.site.register(UserProfile)
+admin.site.register(Skill)
