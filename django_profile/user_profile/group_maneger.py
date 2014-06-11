199b52c59e1960db36d5a54f9f6dc3638d5d1004
Nima Nowroozi <nim4n@yahoo.com>
2014-06-12 00:13:38 +0430

    define GroupManeger class

---
+++ django_profile/user_profile/group_maneger.py
@@ -0,0 +1,23 @@
+
+class GroupManeger(object):
+	def showAllGroups():
+		pass
+	def addPremission():
+		pass
+	def removePremission():
+		pass
+	def showAllPremissions():
+		pass
+	def removeAllPremissions():
+		pass
+	def copyUser():
+		pass
+	def addUser():
+		pass
+	def removeUser():
+		pass
+	def getGroupByName():
+		pass 
+	def getAllPremissions()
+		pass
+		
