fd3646222f11b6ad8429cb833092ecfd0bd94f19
niman <nim4n@yahoo.com>
2014-06-12 18:24:19 +0430

    createSuperUser in UserManager

--- django_profile/user_profile/user_manager.py
+++ django_profile/user_profile/user_manager.py
@@ -1,17 +1,24 @@
 from django.contrib.auth.models import User
+
 class UserManager(object):
     def addNewUser(self,**kwargs):
         '''
-        >>>addNewUser(username='nima',email='nim4n@yahoo.com')
+        >>>addNewUser(username='nima',email='nim4n@yahoo.com',password='nima')
+        >>>
         '''
         userObj = User.objects.create_user(username=kwargs['username'],email=kwargs['email'],password=kwargs['password'])
         return obj
+    def createSuperUser(self,**kwargs):
+        userObj = User.objects.create_superuser(username=kwargs['username'],email=kwargs['email'],password=kwargs['password'])
+        return userObj
     def changePassword(self, **kwargs):
         userObj=  User.objects.get(kwargs['username'])
         userObj.set_password(kwargs['password'])
         return userObj
     def removeUser(self, **kwargs):
-        userObj = User.objects.get()
+        userObj = User.objects.get(username=kwargs['username'])
+        userObj.set_password(kwargs['password'])
+        return userObj
     def addGroup(self):
         pass
     def removeGroup(self):
